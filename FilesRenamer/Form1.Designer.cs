233ef170e2f36aecaa62401c0b4e99cce9a71812
dextrous2010 <windows_13>
2016-07-31 23:21:31 +0300

    Ready solution

--- FilesRenamer/Form1.Designer.cs
+++ FilesRenamer/Form1.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lableTextPath = new System.Windows.Forms.Label();
+            this.lablePath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 173);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(353, 118);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(311, 173);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(637, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lableTextPath
+            // 
+            this.lableTextPath.AutoSize = true;
+            this.lableTextPath.Location = new System.Drawing.Point(18, 24);
+            this.lableTextPath.Name = "lableTextPath";
+            this.lableTextPath.Size = new System.Drawing.Size(84, 13);
+            this.lableTextPath.TabIndex = 5;
+            this.lableTextPath.Text = "Шлях до папки:";
+            // 
+            // lablePath
+            // 
+            this.lablePath.AutoSize = true;
+            this.lablePath.Location = new System.Drawing.Point(103, 20);
+            this.lablePath.Name = "lablePath";
+            this.lablePath.Size = new System.Drawing.Size(0, 13);
+            this.lablePath.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Файли поточної папки:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Перейменовані файли";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Тип файлів:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(521, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Location = new System.Drawing.Point(284, 337);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(113, 31);
+            this.buttonRename.TabIndex = 12;
+            this.buttonRename.Text = "Перейменувати";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 306);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(649, 25);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(673, 379);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lablePath);
+            this.Controls.Add(this.lableTextPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Перейменувач файлів";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lableTextPath;
+        private System.Windows.Forms.Label lablePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- FilesRenamer/Form1.cs
+++ FilesRenamer/Form1.cs
@@ -7,14 +7,146 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FilesRenamer
 {
     public partial class Form1 : Form
     {
+
+        static String dirPath, fileType;
+        static DirectoryInfo dirInfo;
+        FileInfo[] files;
+
+        
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        public void DisplayFolderFiles()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            foreach (FileInfo fileName in files)
+            {
+                listBox1.Items.Add(fileName);
+            }
+        }
+
+        public void DisplayFolderFiles(String fileType)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            foreach (FileInfo fileName in files)
+            {
+                String fileExtension = Path.GetExtension(fileName.Name);
+
+                if (fileExtension == fileType)
+                {
+                    listBox1.Items.Add(fileName);
+                }
+            }
+        }
+
+
+        public void PopulateCBWithFilesExetensions()
+        {
+            comboBox1.Items.Clear();
+
+            foreach (FileInfo fileName in files)
+            {
+                String fileExtension = Path.GetExtension(fileName.Name);
+                if (!comboBox1.Items.Contains(fileExtension))
+                {
+                    comboBox1.Items.Add(fileExtension);
+                }
+            }
+        }
+
+
+        public void RenameFiles() 
+        {
+            Random random = new Random();
+
+            string folderName = "Renamed";
+            Directory.CreateDirectory(dirPath + @"\" + folderName);
+
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = files.Length;
+            progressBar1.Step = 1;
+            progressBar1.Value = 1;
+
+            foreach (FileInfo fileName in files)
+            {
+
+                if (Path.GetExtension(fileName.Name) == (String)comboBox1.SelectedItem || (String)comboBox1.SelectedItem == null)
+                {
+                    String oldFileName = dirPath + @"\" + fileName.Name;
+                    String newFileName = random.Next(1, 199) + "_" + fileName.Name;
+                    String newFilePath = dirPath + @"\" + folderName + @"\" + newFileName;
+
+                    File.Copy(oldFileName, newFilePath, true);
+
+                    listBox2.Items.Add(newFileName);
+                }
+
+                progressBar1.PerformStep();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            if(fbd.SelectedPath != "") 
+            {
+                textBox1.Text = fbd.SelectedPath;
+
+                dirPath = fbd.SelectedPath;
+                dirInfo = new DirectoryInfo(dirPath);
+                files = dirInfo.GetFiles();
+
+                PopulateCBWithFilesExetensions();
+
+                if (comboBox1.SelectedItem != null)
+                {
+                    DisplayFolderFiles((String)comboBox1.SelectedItem);
+                }
+                else
+                {
+                    DisplayFolderFiles();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != "") 
+            {
+                DisplayFolderFiles((String)comboBox1.SelectedItem);
+            }
+
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            if (dirPath != null)
+            {
+                RenameFiles();
+            }
+            else 
+            {
+                MessageBox.Show("Оберіть шлях до папки!");
+            }
+            
+        }
     }
+
 }
